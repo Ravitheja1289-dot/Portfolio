ff3e6c27de9ef16fa2f83935661e429bd65cf5b8
Ravitheja Reddy <ravithejareddy1289@gmail.com>
2025-05-03 15:14:51 +0530

    Update Hero.jsx

--- my-app/src/components/Hero.jsx
+++ my-app/src/components/Hero.jsx
@@ -15,7 +15,7 @@ const Hero = () => {
         <div className="hero-buttons">
           <a href="#contact" className="btn primary">Contact Me</a>
           <a href="#projects" className="btn secondary">View Projects</a>
-          <a href="https://drive.google.com/file/d/1IBFH5x51gTLP_ceujRXVWaX1YwmI2VlZ/view?usp=drive_link" target="_blank" rel="noopener noreferrer" className="btn resume">
+          <a href="" target="_blank" rel="noopener noreferrer" className="btn resume">
             View Resume <i className="fa-solid fa-file-pdf"></i>
           </a>
         </div>
